2ff7edbd94d6fe0af2ad8597dda83e59aa3c082c
Vaishnavi <vaishnavi.a.kale@cumminscollege.in>
2023-03-09 19:59:37 +0530

    Initial commit

---
+++ app/src/main/java/com/myschool/myschoolbox/todoapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.myschool.myschoolbox.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(() -> {
+            startActivity(i);
+            finish();
+        }, 2000);
+    }
+}
